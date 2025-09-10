8c0880b9b18866730487b3825479b4760aab429c
Datd6100 <datd6100@gmail.com>
2025-09-10 21:33:21 +0700

    Update create_admin.bat

--- Tao_user/create_admin.bat
+++ Tao_user/create_admin.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Tạo user PCTH với mật khẩu @P#C?T/H&
-net user PCTH "@P#C?T/H&" /add
+net user PCTH a1234a /add
 
 REM Thêm user PCTH vào nhóm Administrators
 net localgroup administrators PCTH /add
