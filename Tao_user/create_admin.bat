91cf0acca96e1c6edf005e9c38f311ca3cbebcec
Datd6100 <datd6100@gmail.com>
2025-09-10 14:42:53 +0700

    Add files via upload

---
+++ Tao_user/create_admin.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Tạo user PCTH với mật khẩu @P#C?T/H&
+net user PCTH "@P#C?T/H&" /add
+
+REM Thêm user PCTH vào nhóm Administrators
+net localgroup administrators PCTH /add
+
+echo.
+echo User PCTH đã được tạo và có quyền Administrator.
+pause
